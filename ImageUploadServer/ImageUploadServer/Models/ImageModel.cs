a18a281fe090a5b40254001f7efbd545b20aca04
ArsenMalkh <arsen.malkhasyan@phystech.edu>
2022-05-26 20:49:56 +0300

    add

---
+++ ImageUploadServer/ImageUploadServer/Models/ImageModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUploading.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Upload Name")]
+        public string? ImageName { get; set; }
+
+        [NotMapped]  
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Upload File")]
+        public IFormFile? ImageFile { get; set; }
+
+    }
+}
